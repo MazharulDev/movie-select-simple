c7aaab1be68a0b2cb1d93f0b231771be0a58c7dc
Mazharul islam <miforbd@gmail.com>
2022-03-25 22:58:13 +0600

    side bar sticky added

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,11 +3,13 @@ import './Sidebar.css'
 const Sidebar = (props) => {
     return (
         <div className='sidebar'>
+            <div className='sidebar-div'>
             <h2>Selected Movies</h2>
             <div>
                 {
                     props.name.map(nam=><p key={nam.id}>{nam.name}</p>)
                 }
+            </div>
             </div>
         </div>
     );
