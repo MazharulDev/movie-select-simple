1fe1219d76dd1f7e3ae187ac52d214bc6df4d23a
Mazharul islam <miforbd@gmail.com>
2022-03-26 15:09:33 +0600

    by default select 1 for you button disabled

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -10,7 +10,7 @@ const Sidebar = ({name,randomMovie,randomData,clearData}) => {
                         name.map(nam=><p className='sidebar-align' key={nam.id}><img className='sidebar-img' src={nam.img} alt=''/>{nam.name}</p>)
                     }
                 </div>
-                <button onClick={randomMovie}>Select for You</button> <br />
+                <button disabled={name==''} onClick={randomMovie}>Select 1 for You</button> <br />
                 <p>Selected for you: <img src={randomData.img} alt="" /> {randomData.name}</p>
                 <button onClick={()=>clearData(name)}>Choose Again</button>
             </div>
